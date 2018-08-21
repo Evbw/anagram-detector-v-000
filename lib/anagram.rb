6af6c2214b50723c93cf92fd8b82011d03d9e28e
Everett <everett.bowline@gmail.com>
2018-08-21 01:23:05 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -18,3 +18,19 @@ class Anagram
     array1
   end
 end
+
+class Anagram
+  attr_accessor :initialized_word
+  
+  no_matches = []
+  
+  def match(possible_anagrams)
+    possible_anagrams.each do |anagrams|
+    if anagrams.split.sort == :initialized_word.split.sort
+      anagrams
+      elsif
+      no_matches
+  end
+end
+end
+end
